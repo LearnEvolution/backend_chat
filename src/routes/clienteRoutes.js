b2aec8f92989c289396eca0e7e7b0821733b7f04
psh.evolution <psh.evolution@gmail.com>
2025-12-09 19:34:10 -0300

    Fix export default in clienteRoutes.js

--- src/routes/clienteRoutes.js
+++ src/routes/clienteRoutes.js
@@ -1,14 +1,3 @@
-/*import express from "express";
-import clienteController from "../controllers/clienteController.js";
-import authMiddleware from "../middlewares/auth.js";
-
-const router = express.Router();
-
-router.post("/", authMiddleware, clienteController.criar);
-router.get("/", authMiddleware, clienteController.listar);
-
-export default router;
-*/
 import express from "express";
 import { createCliente, getClientes } from "../controllers/clienteController.js";
 
@@ -17,4 +6,4 @@ const router = express.Router();
 router.post("/", createCliente);
 router.get("/", getClientes);
 
-export default router;  // ← AQUI É O QUE ESTÁ FALTANDO
+export default router;
